f6edaa5653750fdf6d39ff280c6b23db8dee5e2e
Ariello <Ariello@DESKTOP-SMD1DHH>
2026-08-31 16:56:31 +0800

    Add StudentProfile Visual Studio Project

---
+++ StudentProfile/StudentProfile/StudentProfile.cs
@@ -0,0 +1,15 @@
+namespace StudentProfile
+{
+    public partial class StudentProfile : Form
+    {
+        public StudentProfile()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClick_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ariello V. Del Castillo", "Student Profile");
+        }
+    }
+}
