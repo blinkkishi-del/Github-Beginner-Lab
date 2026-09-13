94c9a89bbd64bfc4035e3289af82f2f73443292f
Ariello <Ariello@DESKTOP-SMD1DHH>
2026-09-13 20:34:44 +0800

    Implement PhoneNumber field (#2)

--- StudentProfile/StudentProfile/obj/Debug/net10.0-windows/StudentProfile.AssemblyInfo.cs
+++ StudentProfile/StudentProfile/obj/Debug/net10.0-windows/StudentProfile.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentProfile")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64e9402e2a0f598e7a7a258a75a0a634c16a4682")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d2cba7bcdcb1282f5fad8d26bf3b95b01531a39")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentProfile")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentProfile")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
